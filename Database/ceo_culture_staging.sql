59203c69d02ea7acea53e965857c548972184e7d
Jeeho <12766891+jeehobdata@users.noreply.github.com>
2020-06-03 21:57:28 -0400

    Update ceo_culture_staging.sql

--- Database/ceo_culture_staging.sql
+++ Database/ceo_culture_staging.sql
@@ -429,7 +429,7 @@ COLLATE 'utf8mb4_0900_ai_ci';
  
 -- import data into 2019 tables -------------------------------------------------------------
 -- Import data into ceo_bio_2019 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CEOs_info_2019_v1(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../CEOs_info_2019_v1(Apr_25_2020).csv' 
 INTO TABLE ceo_bio_2019 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -437,21 +437,21 @@ IGNORE 1 ROWS;
 
 
 -- Import data into fg500_ceo_2019 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_CEOs_2019_All(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_CEOs_2019_All(Apr_25_2020).csv' 
 INTO TABLE fg500_ceo_2019 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_company_2019 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_2019_20200219001549.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_2019_20200219001549.csv' 
 INTO TABLE fg500_company_2019
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into financial_values_2019 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/key_financials_and_profit_ratio_all_2019_v1.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../key_financials_and_profit_ratio_all_2019_v1.csv' 
 INTO TABLE financial_values_2019
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -462,28 +462,28 @@ IGNORE 1 ROWS;
 
 -- import data into 2018 tables -------------------------------------------------------------
 -- Import data into ceo_bio_2018 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CEOs_info_2018_v1(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../CEOs_info_2018_v1(Apr_25_2020).csv' 
 INTO TABLE ceo_bio_2018 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_ceo_2018 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_CEOs_2018_v2(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_CEOs_2018_v2(Apr_25_2020).csv' 
 INTO TABLE fg500_ceo_2018 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_company_2018 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_2018_20200414014002.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../Uploads/fg500_2018_20200414014002.csv' 
 INTO TABLE fg500_company_2018
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into financial_values_2018 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/key_financials_and_profit_ratio_all_2018_v1.csv' 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/.../key_financials_and_profit_ratio_all_2018_v1.csv' 
 INTO TABLE financial_values_2018
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -494,28 +494,28 @@ IGNORE 1 ROWS;
 
 -- import data into 2017 tables -------------------------------------------------------------
 -- Import data into ceo_bio_2017 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CEOs_info_2017_v1(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../CEOs_info_2017_v1(Apr_25_2020).csv' 
 INTO TABLE ceo_bio_2017 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_ceo_2017 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_CEOs_2017_v1(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_CEOs_2017_v1(Apr_25_2020).csv' 
 INTO TABLE fg500_ceo_2017 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_company_2017 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_2017_20200414020411.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_2017_20200414020411.csv' 
 INTO TABLE fg500_company_2017
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into financial_values_2017 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/key_financials_and_profit_ratio_all_2017_v1.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../key_financials_and_profit_ratio_all_2017_v1.csv' 
 INTO TABLE financial_values_2017
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -526,28 +526,28 @@ IGNORE 1 ROWS;
 
 -- import data into 2016 tables -------------------------------------------------------------
 -- Import data into ceo_bio_2016 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CEOs_info_2016_v1(Apr_25_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../CEOs_info_2016_v1(Apr_25_2020).csv' 
 INTO TABLE ceo_bio_2016 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_ceo_2016 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_CEOs_2016_v2(May_3_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_CEOs_2016_v2(May_3_2020).csv' 
 INTO TABLE fg500_ceo_2016 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_company_2016 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_2016_20200414023501.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_2016_20200414023501.csv' 
 INTO TABLE fg500_company_2016
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into financial_values_2016 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/key_financials_and_profit_ratio_all_2016_v1.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../key_financials_and_profit_ratio_all_2016_v1.csv' 
 INTO TABLE financial_values_2016
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -558,28 +558,28 @@ IGNORE 1 ROWS;
 
 -- import data into 2015 tables -------------------------------------------------------------
 -- Import data into ceo_bio_2015 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CEOs_info_2015_v1(May_5_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../CEOs_info_2015_v1(May_5_2020).csv' 
 INTO TABLE ceo_bio_2015 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_ceo_2015 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_CEOs_2015_v1(May_5_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/.../fg500_CEOs_2015_v1(May_5_2020).csv' 
 INTO TABLE fg500_ceo_2015 
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into fg500_company_2015 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fg500_2015_20200505022239.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../fg500_2015_20200505022239.csv' 
 INTO TABLE fg500_company_2015
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Import data into financial_values_2015 table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/key_financials_and_profit_ratio_all_2015_v1.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../key_financials_and_profit_ratio_all_2015_v1.csv' 
 INTO TABLE financial_values_2015
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -593,7 +593,7 @@ IGNORE 1 ROWS;
 
 
 -- Import data into globe_leadership table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/GLOBE_Phase_2_Aggregated_Leadership_v2.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../GLOBE_Phase_2_Aggregated_Leadership_v2.csv' 
 INTO TABLE globe_leadership
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -601,7 +601,7 @@ IGNORE 1 ROWS;
 
 
 -- Import data into globe_societal_culture table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/GLOBE_Phase_2_Aggregated_Societal_Culture_v2.csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../GLOBE_Phase_2_Aggregated_Societal_Culture_v2.csv' 
 INTO TABLE globe_societal_culture
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
@@ -609,7 +609,7 @@ IGNORE 1 ROWS;
 
 
 -- Import data into hofstede table
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Hofstede_6D_v3(Apr_18_2020).csv' 
+LOAD DATA INFILE 'C:/ProgramData/.../Hofstede_6D_v3(Apr_18_2020).csv' 
 INTO TABLE hofstede
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
